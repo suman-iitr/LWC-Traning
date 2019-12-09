f7fc30a7aef93085da721045dc2ee2b77b2a6768
suman-iitr <49260837+suman-iitr@users.noreply.github.com>
2019-12-08 21:14:27 -0500

    Apex Wire Method To Property

---
+++ Slide 30 apexWireMethodToProperty/apexWireMethodToProperty.js
@@ -0,0 +1,7 @@
+// apexWireMethodToProperty.js
+import { LightningElement, wire } from 'lwc';
+import getContactList from '@salesforce/apex/ContactController.getContactList';
+
+export default class ApexWireMethodToProperty extends LightningElement {
+    @wire(getContactList) contacts;
+}
